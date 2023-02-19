77de3e3f57b9ecab9afc45e973cd8d536e64be2a
RohaNoorRahman <tuhin222666rohan@gmail.com>
2023-02-19 21:53:00 +0600

    freatures and testimonial are complete design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,16 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     screens:{
-      mobile:'300px',
       sm:'480px',
       md:'768px',
-      lg:'1100px',
+      lg:'976px',
       xl:'1440px'
     },
     extend: {
       colors:{
-        brigthRed:'hsl(12,88%,59%)',
+        brightRed:'hsl(12,88%,59%)',
         brightRedLight: 'hsl(12,88%,69%)',
-        brightredSupLight:'hsl(12,88%,95%)',
+        brightredSupLight:'hsl(12, 88%, 95%)',
         darkBlue:'hsl(228,39%,23%)',
         darkGrayishBlue:'hsl(227,12%,61%)',
         veryDarkBlue:'hsl(233,12%,13%)',
